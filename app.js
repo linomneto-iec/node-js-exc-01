6455d4de0fd64204380b28cd843dd2d752f1e253
Lino Melhado Neto <linomneto@gmail.com>
2022-03-19 15:05:52 -0300

    fix content type

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 5000
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Type', 'text/html; charset=utf-8');
   res.end('<html><head><title>Node.JS - Exercício 01</title></head><body><h2>Node.JS - Exercício 01</h2><h1>Lino Antonio Melhado Neto</h2></body></html>');
 });
 
